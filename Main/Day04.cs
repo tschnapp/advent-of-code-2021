52f32885d785478a2fcb933671a519b528a9a041
Brian Schnapp <brian@tcgplayer.com>
2021-12-05 13:44:26 -0500

    Day04 & Day05

---
+++ Main/Day04.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Main
+{
+    class Day04 : Day
+    {
+        [TestCase("Test01", "4512")]
+        [TestCase("Final", "82440")]
+        public override string Part01(string[] rawInput)
+        {
+            var input = rawInput.ToList();
+            var bingoInput = input[0].Split(',').Select(Int32.Parse).ToList();
+            List<List<int>> cards = new List<List<int>> { new List<int> { } };
+
+            // Get data
+            var currentCard = 0;
+            for (int l = 2; l < input.Count(); l++)
+            {
+                if (l % 6 == 1)
+                {
+                    cards.Add(new List<int> { });
+                    currentCard++;
+                }
+                else
+                {
+                    cards[currentCard].AddRange(input[l].Trim().Replace("  ", " ").Split(' ').Select(Int32.Parse));
+                }
+            }
+
+            // Create Checks list
+            List<List<int>> checks = new List<List<int>> { };
+
+            for (int c = 0; c < cards.Count; c++)
+                checks.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+
+
+            // Mark cards with number
+            for (int n = 0; n < bingoInput.Count(); n++)
+            {
+                for (int c = 0; c < cards.Count(); c++)
+                {
+                    var numberPositionOnCard = cards[c].FindIndex(a => a == bingoInput[n]);
+                    if (numberPositionOnCard != -1)
+                    {
+                        checks[c][numberPositionOnCard] = 1;
+
+                        // Check if card is a winner
+                        if (checkForWinner(checks[c]))
+                            return (getWinnerTotal(checks[c], cards[c]) * cards[c][numberPositionOnCard]).ToString();
+                    }
+                }
+            }
+
+            return "";
+        }
+
+        private bool checkForWinner(List<int> cardChecks)
+        {
+            var isWinner = false;
+
+            // Check horizontal rows
+            for (int n = 0; n < cardChecks.Count(); n += 5)
+                if (cardChecks.GetRange(n, 5).Sum() == 5)
+                    isWinner = true;
+
+            // Check vertical rows
+            for (int n = 0; n < Math.Sqrt(cardChecks.Count()); n++)
+                if (cardChecks.Where((v, i) => i % 5 == n).Sum() == 5)
+                    isWinner = true;
+
+            return isWinner;
+        }
+
+        private int getWinnerTotal(List<int> cardChecks, List<int> cardNumbers)
+        {
+            var total = 0;
+
+            for(int n =0; n < cardChecks.Count(); n++)
+                if (cardChecks[n] != 1)
+                    total += cardNumbers[n];
+
+            return total;
+        }
+
+        [TestCase("Test01", "1924")]
+        [TestCase("Final", "20774")]
+        public override string Part02(string[] rawInput)
+        {
+            var input = rawInput.ToList();
+            var bingoInput = input[0].Split(',').Select(Int32.Parse).ToList();
+            List<List<int>> cards = new List<List<int>> { new List<int> { } };
+            List<int> winners = new List<int> { };
+
+            // Get data
+            var currentCard = 0;
+            for (int l = 2; l < input.Count(); l++)
+            {
+                if (l % 6 == 1)
+                {
+                    cards.Add(new List<int> { });
+                    currentCard++;
+                }
+                else
+                {
+                    cards[currentCard].AddRange(input[l].Trim().Replace("  ", " ").Split(' ').Select(Int32.Parse));
+                }
+            }
+
+            // Create Checks list
+            List<List<int>> checks = new List<List<int>> { };
+
+            for (int c = 0; c < cards.Count; c++)
+                checks.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+
+
+            // Mark cards with number
+            for (int n = 0; n < bingoInput.Count(); n++)
+            {
+                for (int c = 0; c < cards.Count(); c++)
+                {
+                    if (!winners.Contains(c))
+                    {
+                        var numberPositionOnCard = cards[c].FindIndex(a => a == bingoInput[n]);
+                        if (numberPositionOnCard != -1)
+                        {
+                            checks[c][numberPositionOnCard] = 1;
+
+                            // Check if card is a winner
+                            if (checkForWinner(checks[c]))
+                            {
+                                winners.Add(c);
+                                if (winners.Count() == cards.Count())
+                                    return (getWinnerTotal(checks[c], cards[c]) * cards[c][numberPositionOnCard]).ToString();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return "";
+        }
+    }
+}
---
+++ Main/Day05.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Main
+{
+    class Day05 : Day
+    {
+        [TestCase("Test01", "5")]
+        [TestCase("Final", "7468")]
+        public override string Part01(string[] rawInput)
+        {
+            var input = rawInput.ToList();
+            List<Point> startPoints = new List<Point>();
+            List<Point> endPoints = new List<Point>();
+            var twoLinesOverlapCount = 0;
+
+            // Get points
+            foreach (string i in input)
+            {
+                var coordindates = i.Split(" -> ");
+                var coordinate = coordindates[0].Split(',').Select(Int32.Parse).ToList();
+                startPoints.Add(new Point(coordinate[0], coordinate[1]));
+
+                coordinate = coordindates[1].Split(',').Select(Int32.Parse).ToList();
+                endPoints.Add(new Point(coordinate[0], coordinate[1]));
+            }
+
+
+            var mapMaxX = startPoints.Max(x => x.X) > endPoints.Max(x => x.X) ? startPoints.Max(x => x.X) + 1 : endPoints.Max(x => x.X) + 1;
+            var mapMaxY = startPoints.Max(y => y.Y) > endPoints.Max(y => y.Y) ? startPoints.Max(y => y.Y) + 1 : endPoints.Max(y => y.Y) + 1;
+            int[,] map = new int[mapMaxX, mapMaxY];
+
+            // Populate map
+            for (int i = 0; i < startPoints.Count(); i++)
+            {
+                if (startPoints[i].X == endPoints[i].X || startPoints[i].Y == endPoints[i].Y)
+                {
+                    if (startPoints[i].Y == endPoints[i].Y)             // Horizontal line
+                    {
+                        var startingValue = startPoints[i].X < endPoints[i].X ? startPoints[i].X : endPoints[i].X;
+                        var lineLength = Math.Abs(startPoints[i].X - endPoints[i].X) + startingValue;
+
+                        for (int x = startingValue; x <= lineLength; x++)
+                        {
+                            map[x, startPoints[i].Y]++;
+                        }
+                    }
+                    else if (startPoints[i].X == endPoints[i].X)             // Vertical line
+                    {
+                        var startingValue = startPoints[i].Y < endPoints[i].Y ? startPoints[i].Y : endPoints[i].Y;
+                        var lineLength = Math.Abs(startPoints[i].Y - endPoints[i].Y) + startingValue;
+
+                        for (int y = startingValue; y <= lineLength; y++)
+                        {
+                            map[startPoints[i].X, y]++;
+                        }
+                    }
+                }
+            }
+
+            // Get vent >= 2 counts
+            for (int y = 0; y < mapMaxY; y++)
+            {
+                //Console.WriteLine();
+                for (int x = 0; x < mapMaxX; x++)
+                {
+                    //Console.Write(map[x, y]);
+                    if (map[x, y] >= 2)
+                        twoLinesOverlapCount++;
+                }
+            }
+            
+            return twoLinesOverlapCount.ToString();
+        }
+
+        [TestCase("Test01", "12")]
+        [TestCase("Final", "22364")]
+        public override string Part02(string[] rawInput)
+        {
+            var input = rawInput.ToList();
+            List<Point> startPoints = new List<Point>();
+            List<Point> endPoints = new List<Point>();
+            var twoLinesOverlapCount = 0;
+
+            // Get points
+            foreach (string i in input)
+            {
+                var coordindates = i.Split(" -> ");
+                var coordinate = coordindates[0].Split(',').Select(Int32.Parse).ToList();
+                startPoints.Add(new Point(coordinate[0], coordinate[1]));
+
+                coordinate = coordindates[1].Split(',').Select(Int32.Parse).ToList();
+                endPoints.Add(new Point(coordinate[0], coordinate[1]));
+            }
+
+            var mapMaxX = startPoints.Max(x => x.X) > endPoints.Max(x => x.X) ? startPoints.Max(x => x.X) + 1 : endPoints.Max(x => x.X) + 1;
+            var mapMaxY = startPoints.Max(y => y.Y) > endPoints.Max(y => y.Y) ? startPoints.Max(y => y.Y) + 1 : endPoints.Max(y => y.Y) + 1;
+            int[,] map = new int[mapMaxX, mapMaxY];
+
+            // Populate map
+            for (int i = 0; i < startPoints.Count(); i++)
+            {
+                if (startPoints[i].Y == endPoints[i].Y)             // Horizontal line
+                {
+                    var startingValueX = startPoints[i].X < endPoints[i].X ? startPoints[i].X : endPoints[i].X;
+                    var lineLength = Math.Abs(startPoints[i].X - endPoints[i].X) + startingValueX;
+
+                    for (int x = startingValueX; x <= lineLength; x++)
+                    {
+                        map[x, startPoints[i].Y]++;
+                    }
+                }
+                else if (startPoints[i].X == endPoints[i].X)             // Vertical line
+                {
+                    var startingValueY = startPoints[i].Y < endPoints[i].Y ? startPoints[i].Y : endPoints[i].Y;
+                    var lineLength = Math.Abs(startPoints[i].Y - endPoints[i].Y) + startingValueY;
+
+                    for (int y = startingValueY; y <= lineLength; y++)
+                    {
+                        map[startPoints[i].X, y]++;
+                    }
+                }
+                else                                                    // Diagnal line
+                {
+                    var startingValueX = startPoints[i].X < endPoints[i].X ? startPoints[i].X : endPoints[i].X;
+                    var startingValueY = startPoints[i].X < endPoints[i].X ? startPoints[i].Y : endPoints[i].Y;     // flip if x flipped
+                    var lineLength = Math.Abs(startPoints[i].X - endPoints[i].X) + startingValueX;
+                    var y = startingValueY;
+                    var yDirection = startPoints[i].Y < endPoints[i].Y ? 1 : -1;
+                    yDirection *= startPoints[i].X < endPoints[i].X ? 1 : -1;                                       // if the line flipped, reverse the direction
+
+                    for (int x = startingValueX; x <= lineLength; x++)
+                    {
+                        map[x, y]++;
+                        y += yDirection;
+                    }
+                }
+            }
+
+            // Get vent >= 2 counts
+            for (int y = 0; y < mapMaxY; y++)
+            {
+                //Console.WriteLine();
+                for (int x = 0; x < mapMaxX; x++)
+                {
+                    //Console.Write(map[x, y]);
+                    if (map[x, y] >= 2)
+                        twoLinesOverlapCount++;
+                }
+            }
+
+            return twoLinesOverlapCount.ToString();
+        }
+    }
+}
