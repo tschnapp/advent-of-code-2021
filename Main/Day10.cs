5fdc9acf87aa68ec8efbec630674150edf553ef1
Brian Schnapp <brian@tcgplayer.com>
2021-12-10 13:01:38 -0500

    Day10

---
+++ Main/Day10.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Main
+{
+    class Day10 : Day
+    {
+        [TestCase("Test01", "26397")]
+        [TestCase("Final", "167379")]
+        public override string Part01(string[] rawInput)
+        {
+            var input = rawInput.ToList();
+            var stack = new List<string>();
+            var total = 0;
+
+            foreach (string error in input)
+            {
+                var subTotal = 0;
+
+                foreach (char c in error)
+                {
+                    if ("([{<".Contains(c.ToString()))                                          // opening char
+                    {
+                        stack.Add(c.ToString());
+                    }
+                    else if (")]}>".Contains(c.ToString()))                                     // closing char
+                    {
+                        if ("([{<".IndexOf(stack.Last()) != ")]}>".IndexOf(c))                  // bad char
+                        {
+                            subTotal += new int[] { 3, 57, 1197, 25137 }[")]}>".IndexOf(c)];    // add to total
+                            break;
+                        }
+                        stack.RemoveAt(stack.Count() - 1);
+                    }
+                }
+
+                total += subTotal;
+            }
+
+            return total.ToString();
+        }
+
+        [TestCase("Test01", "288957")]
+        [TestCase("Final", "2776842859")] // too low
+        public override string Part02(string[] rawInput)
+        {
+            var input = rawInput.ToList();
+            var totals = new List<long>();
+
+            foreach (string error in input)
+            {
+                var stack = new List<string>();
+                var isCorrupt = false;
+                long subTotal = 0;
+
+                foreach (char c in error)
+                {
+                    if ("([{<".Contains(c.ToString()))                                          // opening char
+                    {
+                        stack.Add(c.ToString());
+                    }
+                    else if (")]}>".Contains(c.ToString()))                                     // closing char
+                    {
+                        if ("([{<".IndexOf(stack.Last()) != ")]}>".IndexOf(c))                  // bad char, skip this one
+                        {
+                            isCorrupt = true;
+                            break;
+                        }
+
+                        stack.RemoveAt(stack.Count() - 1);
+                    }
+                }
+
+                if (!isCorrupt)
+                {
+                    for (int c = stack.Count() - 1; c >= 0; c--)
+                    {
+                        subTotal = (subTotal * 5) + "([{<".IndexOf(stack[c]) + 1;               // get sub total
+                    }
+
+                    totals.Add(subTotal);                                                       // add sub total to total list 
+                }
+            }
+
+            return totals.OrderBy(x => x).ToList()[totals.Count() / 2].ToString();              // return middle total
+        }
+    }
+}
