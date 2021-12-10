bef497d7642ea480c52815d455e3e757137cb5a0
Brian Schnapp <brian@tcgplayer.com>
2021-12-10 13:28:26 -0500

    Tweaked Day10

--- Main/Day10.cs
+++ Main/Day10.cs
@@ -14,6 +14,8 @@ namespace Main
         {
             var input = rawInput.ToList();
             var stack = new List<string>();
+            var openingChars = "([{<";
+            var closingChars = ")]}>";
             var total = 0;
 
             foreach (string error in input)
@@ -22,17 +24,18 @@ namespace Main
 
                 foreach (char c in error)
                 {
-                    if ("([{<".Contains(c.ToString()))                                          // opening char
+                    if (openingChars.Contains(c.ToString()))                                        // opening char
                     {
                         stack.Add(c.ToString());
                     }
-                    else if (")]}>".Contains(c.ToString()))                                     // closing char
+                    else if (closingChars.Contains(c.ToString()))                                   // closing char
                     {
-                        if ("([{<".IndexOf(stack.Last()) != ")]}>".IndexOf(c))                  // bad char
+                        if (openingChars.IndexOf(stack.Last()) != closingChars.IndexOf(c))          // bad char
                         {
-                            subTotal += new int[] { 3, 57, 1197, 25137 }[")]}>".IndexOf(c)];    // add to total
+                            subTotal += new int[] { 3, 57, 1197, 25137 }[closingChars.IndexOf(c)];  // add to total
                             break;
                         }
+
                         stack.RemoveAt(stack.Count() - 1);
                     }
                 }
@@ -48,6 +51,8 @@ namespace Main
         public override string Part02(string[] rawInput)
         {
             var input = rawInput.ToList();
+            var openingChars = "([{<";
+            var closingChars = ")]}>";
             var totals = new List<long>();
 
             foreach (string error in input)
@@ -58,13 +63,13 @@ namespace Main
 
                 foreach (char c in error)
                 {
-                    if ("([{<".Contains(c.ToString()))                                          // opening char
+                    if (openingChars.Contains(c.ToString()))                                        // opening char
                     {
                         stack.Add(c.ToString());
                     }
-                    else if (")]}>".Contains(c.ToString()))                                     // closing char
+                    else if (closingChars.Contains(c.ToString()))                                   // closing char
                     {
-                        if ("([{<".IndexOf(stack.Last()) != ")]}>".IndexOf(c))                  // bad char, skip this one
+                        if (openingChars.IndexOf(stack.Last()) != closingChars.IndexOf(c))          // bad char, skip this one
                         {
                             isCorrupt = true;
                             break;
@@ -78,14 +83,14 @@ namespace Main
                 {
                     for (int c = stack.Count() - 1; c >= 0; c--)
                     {
-                        subTotal = (subTotal * 5) + "([{<".IndexOf(stack[c]) + 1;               // get sub total
+                        subTotal = (subTotal * 5) + openingChars.IndexOf(stack[c]) + 1;             // get sub total
                     }
 
-                    totals.Add(subTotal);                                                       // add sub total to total list 
+                    totals.Add(subTotal);                                                           // add sub total to total list 
                 }
             }
 
-            return totals.OrderBy(x => x).ToList()[totals.Count() / 2].ToString();              // return middle total
+            return totals.OrderBy(x => x).ToList()[totals.Count() / 2].ToString();                  // return middle total
         }
     }
 }
